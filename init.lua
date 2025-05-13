2713fff9b92cbff80e1c2bb6aafebfce888b2ae5
Carpetess <andre.filipe19062003@gmail.com>
2025-05-13 14:24:45 +0100

    Fuck it, imma re write everything

--- init.lua
+++
@@ -1,9 +0,0 @@
-require("config")
-
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-vim.cmd("set relativenumber")
-vim.cmd("set number")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set expandtab")
-
--- lua/plugins/floaterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"voldikss/vim-floaterm",
-	keys = {
-		{ "<leader>tt", ":FloatermToggle cwd <buffer><CR>", desc="Toggle floating terminal" },
-		{ "<leader>tr", ":FloatermToggle cwd <root><CR>", desc="Toggle floating terminal" },
-	},
-}
-
